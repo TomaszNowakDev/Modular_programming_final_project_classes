41a536a41dfdc5567ef129e20e03806d2cde9b48
Tomasz Nowak <tomasz.nowak@mycit.ie>
2021-04-19 18:47:50 +0100

    Added display_numbered() function. Implemented option 2 of main menu.

--- main.py
+++ main.py
@@ -32,9 +32,14 @@ class ResultOfRace:
         return tf
 
 
-def display(items):
+def display_numbered(items):
     for item in range(len(items)):
         print(f"{item+1}. {items[item]}")
+
+
+def display(items):
+    for i in range(len(items)):
+        print(items[i])
 
 
 def reading_races():
@@ -43,33 +48,34 @@ def reading_races():
         return [race.strip() for race in lines]
 
 
-def validation_for_choice(rac, prompt):
-    display(rac)
+def validation_for_choice(minimum, maximum, prompt):
     while True:
         try:
             cho = int(input(prompt))
-            if 0 <= cho <= len(rac):
+            if minimum < cho <= maximum:
                 break
             else:
-                print("Choose one of the options please.")
+                print("Choose one of the following options please.")
         except ValueError:
             print("Numbers only please!")
     return cho
 
 
-with open("Runners.txt") as file_runners:
-    lines_runners = file_runners.readlines()
-    runners = []
-    for line in lines_runners:
-        split_line_runners = line.split(",")
-        name = split_line_runners[0]
-        id_runner = split_line_runners[1].strip()
-        runner = Runner(name, id_runner)
-        runners.append(runner)
+def reading_runners():
+    with open("Runners.txt") as file_runners:
+        lines_runners = file_runners.readlines()
+        runners = []
+        for line in lines_runners:
+            split_line_runners = line.split(",")
+            name = split_line_runners[0]
+            id_runner = split_line_runners[1].strip()
+            runner = Runner(name, id_runner)
+            runners.append(runner)
+    return runners
 
 
-def reading_venues(races, choice1):
-    with open(f"{races[choice1 - 1]}.txt") as file_races:
+def reading_venues(race, choice1):
+    with open(f"{race[choice1 - 1]}.txt") as file_races:
         lines_race_details = file_races.readlines()
         races_details = []
         for lin in lines_race_details:
@@ -83,36 +89,65 @@ def reading_venues(races, choice1):
 
 def main():
     print(MAIN_MENU)
-    try:
-        choice_main = int(input("==>"))
-        while True:
-            races = reading_races()
-            if choice_main == 1:
-                print("(1) Show the results for a race \n===============================")
-                choice1 = validation_for_choice(races, "Choice ==> ")
-                race_details = reading_venues(races, choice1)
-                print(f"Results for {races[choice1 - 1]}\n=======================")
-                for racer in race_details:
-                    print(racer)
-            elif choice_main == 2:
-                print("(2) Add results for a race \n===============================")
-            elif choice_main == 3:
-                print("(3) Show all competitors by county \n===============================")
-            elif choice_main == 4:
-                print("(4) Show the winner of each race \n===============================")
-            elif choice_main == 5:
-                print("(5) Show all the race times for one competitor \n===============================")
-            elif choice_main == 6:
-                print("(6) Show all competitors who have won a race \n===============================")
-            elif choice_main == 7:
-                print("Thank you, Goodbye.")
-                break
-            else:
-                print("Choose one of the options from 1 to 7.")
-            print(MAIN_MENU)
-            choice_main = int(input("==>"))
-    except ValueError:
-        print("Please choose one of the options from main menu.")
+    choice_main = validation_for_choice(0, 7, "==>")
+    while True:
+        races = reading_races()
+        runners = reading_runners()
+
+        if choice_main == 1:
+            print("(1) Show the results for a race \n===============================")
+            display_numbered(races)
+            choice1 = validation_for_choice(0, len(races), "Choice ==> ")
+            race_details = reading_venues(races, choice1)
+            print(f"Results for {races[choice1 - 1]}\n=======================")
+            for racer in race_details:
+                print(racer)
+            years = [race_details[i].time for i in range(len(race_details))]
+            fastest = min(years)
+            winner = [runner.code_run+" won the race." for runner in race_details if runner.time == fastest]
+            print()
+            display(winner)
+        elif choice_main == 2:
+            print("(2) Add results for a race \n===============================")
+            new_race = input("Name of new race location ==> ").capitalize()
+            while True:
+                if new_race not in races:
+                    with open("Races.txt", "a") as file_option2:
+                        print(new_race, file=file_option2)
+                else:
+                    print(f"Data for {new_race} already exists, please enter a different name.")
+                    new_race = input("Name of new race location ==> ").capitalize()
+                with open(new_race.lower() + ".txt", "w") as file_race:
+                    for racer1 in runners:
+                        while True:
+                            try:
+                                time_from_race = int(input(f"What time {racer1.code} got in {new_race} race? ==>"))
+                                if time_from_race > 0:
+                                    print(f"{racer1.code},{time_from_race}", file=file_race)
+                                    break
+                                elif time_from_race == 0:
+                                    print(f"{racer1.code} did not participate or did not finish the {new_race} race.")
+                                    break
+                                else:
+                                    print("Positive numbers only, or number zero if the runner did not participate or"
+                                          + " did not finish the race.")
+                            except ValueError:
+                                print("Numbers only please!")
+                    break
+
+        elif choice_main == 3:
+            print("(3) Show all competitors by county \n===============================")
+        elif choice_main == 4:
+            print("(4) Show the winner of each race \n===============================")
+        elif choice_main == 5:
+            print("(5) Show all the race times for one competitor \n===============================")
+        elif choice_main == 6:
+            print("(6) Show all competitors who have won a race \n===============================")
+        elif choice_main == 7:
+            print("Thank you, Goodbye.")
+            break
+        print(MAIN_MENU)
+        choice_main = validation_for_choice(0, 7, "==>")
 
 
 if __name__ == '__main__':
